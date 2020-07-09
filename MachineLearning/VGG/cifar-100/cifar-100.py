bfad48f84056c57395fae9dd20a26b44ee9bc5da
김동주 <hepheir@gmail.com>
2020-07-10 02:42:55 +0900

    Update cifar-100.py

--- MachineLearning/VGG/cifar-100/cifar-100.py
+++ MachineLearning/VGG/cifar-100/cifar-100.py
@@ -6,8 +6,14 @@ model.summary()
 
 (x_train, y_train), (x_test, y_test) = cifar100.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
-print(f'download complete: trains: {x_train.shape}, tests: {x_test.shape}')
 
-model.fit(x_train, y_train, epochs=8)
+print(f'''
+download complete:
+trains: {x_train.shape},
+tests: {x_test.shape}
+''')
+
+# About batch_size: https://nittaku.tistory.com/293
+model.fit(x_train, y_train, epochs=250, batch_size=1)
 model.evaluate(x_test, y_test)
 model.save('./models/CIFAR-100/trained.model')
