fd3e7a91f8217d9c05294eaf0907b99579afdab8
김동주 <hepheir@gmail.com>
2020-07-10 02:27:59 +0900

    Create cifar-100.py

---
+++ MachineLearning/VGG/cifar-100/cifar-100.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.datasets import cifar100
+
+model = load_model('../models/CIFAR-100/empty.model')
+model.summary()
+
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print(f'download complete: trains: {x_train.shape}, tests: {x_test.shape}')
+
+model.fit(x_train, y_train, epochs=8)
+model.evaluate(x_test, y_test)
+model.save('./models/CIFAR-100/trained.model')
